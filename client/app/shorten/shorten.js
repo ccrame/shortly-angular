89e27f86b8b5d68ad4cec9ea6c7db40c2eaecfcc
Charles Crame <ccrame@gmail.com>
2015-07-31 17:56:23 -0700

    added function to create link and to check if url is valid

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,23 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.sending = false;
+
+  $scope.createLink = function(){
+    $scope.sending = true;
+    Links.postLink($scope.url)
+      .then(function(){
+        $scope.sending = false;
+        $scope.url = "";
+      })
+      .catch(function(err){
+        console.err(err);
+      });
+  };
+
+  var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+
+  $scope.validUrl = function(){
+    return $scope.url ? $scope.url.match(rValidUrl) : true;
+  };
 });
