c4054ee2c276b21485d1bfae5413a01162880449
Charles Crame <ccrame@gmail.com>
2015-08-01 14:53:07 -0700

    implemented addLink function to send a POST request to database

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,22 +3,27 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
   $scope.sending = false;
+  $scope.url = {
+    text: ''
+  };
 
-  $scope.createLink = function(){
-    $scope.sending = true;
-    Links.postLink($scope.url)
-      .then(function(){
-        $scope.sending = false;
-        $scope.url = "";
-      })
-      .catch(function(err){
-        console.err(err);
-      });
+  $scope.addLink = function(){
+    if($scope.validUrl()){
+      $scope.sending = true;
+      Links.postLink($scope.url.text)
+        .then(function(){
+          $scope.sending = false;
+          $scope.url.text = "";
+        })
+        .catch(function(err){
+          console.err(err);
+        });
+    }
   };
 
   var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
 
   $scope.validUrl = function(){
-    return $scope.url ? $scope.url.match(rValidUrl) : true;
+    return $scope.url.text && $scope.url.text.match(rValidUrl);
   };
 });
